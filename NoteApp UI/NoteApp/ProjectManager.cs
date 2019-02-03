5a20d069dae482180542db48c42561caaf9f18a5
Sigrid <devilisdaughter@mail.ru>
2019-02-03 19:18:03 +0700

    создан код для класса ProjectManager

--- NoteApp UI/NoteApp/ProjectManager.cs
+++ NoteApp UI/NoteApp/ProjectManager.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace NoteApp
 {
-    class ProjectManager
+    public static class ProjectManager
     {
+        private static string _PathFile = @"";
+
+        public static void SaveToFile (Project se, string filename)
+        {
+            _PathFile = filename;
+            JsonSerializer serializer = new JsonSerializer();
+            ///<summary>
+            ///открытие потока для записи файла с указанем пути
+            ///</summary>
+            using (StreamWriter sw = new StreamWriter(filename))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                ///<summary>
+                ///вызов сериализации и передача объекта, который хотим сериализовать
+                ///</summary>
+                serializer.Serialize(writer, se);
+            }
+        }
+        public static Project LoadFromFile(string filename)
+        {
+            _PathFile = filename;
+            Project deNote = null;
+            ///<summary>
+            ///если файл отсутствует, создать его и вернуть новый
+            ///</summary>
+            if (!FileStyleUriParser.Exists(filename))
+            {
+                deNote = new Project();
+                SaveToFile(deNote, filename);
+                return deNote;
+            }
+            ///<summary>
+            ///создать экземпляр сериализатора
+            ///</summary>
+            JsonSerializer serializer = new JsonSerializer();
+            ///<summary>
+            ///открыть поток для чтения из файда с указанием пути
+            ///</summary>
+            using (StreamRader sr = new StreamReader(filename))
+            using (JsonReader reader = new JsonTextReader(sr))
+            {
+                ///<summary>
+                ///вызов десериализации и явно преобразуем результат в целевой тип данных
+                ///</summary>
+                deNote = (Project)serializer.Deserialize<Project>(reader);
+            }
+            return deNote;
+        }
     }
 }
