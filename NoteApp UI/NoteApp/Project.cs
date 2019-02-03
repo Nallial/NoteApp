d0279d6cae6172bac5de947b612a7e3458c8d5d8
Sigrid <devilisdaughter@mail.ru>
2019-02-03 18:54:00 +0700

    создан код для класса Project

---
+++ NoteApp UI/NoteApp/Project.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp
+{
+    class Project
+    {
+        public List<Note> Notes = new List<Note>();
+    }
+}
